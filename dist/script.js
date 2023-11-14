94cd479418d8931e6ff77556f9ea169de22218c6
Vladislav Denisov <denisov.ve1997@gmail.com>
2023-11-14 14:38:33 +0400

    request

--- dist/script.js
+++ dist/script.js
@@ -302,6 +302,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_tabs__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./components/tabs */ "./src/js/lib/components/tabs.js");
 /* harmony import */ var _components_accordion__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./components/accordion */ "./src/js/lib/components/accordion.js");
 /* harmony import */ var _components_carousel__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ./components/carousel */ "./src/js/lib/components/carousel.js");
+/* harmony import */ var _services_request__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! ./services/request */ "./src/js/lib/services/request.js");
+
 
 
 
@@ -699,6 +701,46 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.off = function (eventTyp
     }
   } else {
     return this;
+  }
+};
+
+/***/ }),
+
+/***/ "./src/js/lib/services/request.js":
+/*!****************************************!*\
+  !*** ./src/js/lib/services/request.js ***!
+  \****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../core */ "./src/js/lib/core.js");
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.get = async function (url, typeResp = "json") {
+  let res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+  }
+  switch (typeResp) {
+    case "json":
+      return await res.json();
+    case "text":
+      return await res.text();
+    case "blob":
+      return await res.blob();
+  }
+};
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.post = async function (url, data, typeResp = "text") {
+  let res = await fetch(url, {
+    method: "POST",
+    body: data
+  });
+  switch (typeResp) {
+    case "json":
+      return await res.json();
+    case "text":
+      return await res.text();
+    case "blob":
+      return await res.blob();
   }
 };
 
